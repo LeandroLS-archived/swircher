3ae27373f166b0eeb5aeb5e9bc862b10145c50bf
Leandro Lima <llimas@outlook.com>
2021-10-13 20:57:48 -0300

    probably last commit

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -31,7 +33,7 @@ func askUser() (userToCheck string, isFollowedBy string) {
 	fmt.Print("Enter User: ")
 	scanner.Scan()
 	userToCheck = scanner.Text()
-	fmt.Printf("Check if %v has some follower with this name: ", userToCheck)
+	fmt.Printf("Check if %v has some follower with this User Name: ", userToCheck)
 	scanner.Scan()
 	isFollowedBy = scanner.Text()
 	if scanner.Err() != nil {
@@ -53,20 +55,22 @@ func handleErr(err error) {
 func makeRequest(method string, url string, authorization string) []byte {
 	req, err := http.NewRequest(method, url, nil)
 	handleErr(err)
-	req.Header.Add("Authorization", authorization)
+	req.Header.Add("Content-Type", "application/json;charset=utf-8")
+	fmt.Println(req)
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	handleErr(err)
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body), err)
 	handleErr(err)
 	return body
 }
 
-func makeTweet() []byte {
-	baseUrl := fmt.Sprintf("https://api.twitter.com/1.1/statuses/update.json?status=%v", "teste")
+func makeTweet(message string) []byte {
+	baseUrl := fmt.Sprintf("https://api.twitter.com/1.1/statuses/update.json?status=%s", url.QueryEscape(message))
 	fmt.Println(baseUrl)
-	auth0 := "OAuth oauth_consumer_key=\"NYgxnI8X8rsz8i8GJ7AJncNPO\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1634157268\", oauth_nonce=\"hBMb7RQ8PuUjofdwpcAvz7CeE2BZTra1\", oauth_signature=\"xHFeP7FydiFae7SExHGqD%2Bs9F2g%3D\", oauth_token=\"740996939680993280-UdTpnznooFWij3lLd2LCUeKYlfQM8fy\", oauth_version=\"1.0\""
+	auth0 := "super secret token"
 	body := makeRequest("POST", baseUrl, auth0)
 	return body
 }
@@ -81,28 +85,55 @@ func getUserByUserName(userName string) TwitterAPIResponseUserInfo {
 	return twitterData
 }
 
+func getUserChoise() int {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Options:")
+	fmt.Println("1 => Make a Tweet")
+	fmt.Println("2 => See who follows a specific user")
+	fmt.Println("Enter the numbers 1 or 2 to chose what you wanna do:")
+	text, _ := reader.ReadString('\n')
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+	number, err := strconv.Atoi(text)
+	handleErr(err)
+	if number != 1 && number != 2 {
+		fmt.Println("Please inform a number between 1 or 2")
+		getUserChoise()
+	}
+	return number
+}
+
 func main() {
-	body := makeTweet()
-	fmt.Println(string(body))
-	// userToCheck, followedBy := askUser()
-	// bearerToken := os.Getenv("TWITTER_BEARER_TOKEN")
-	// if bearerToken == "" {
-	// 	log.Fatalln("Environment Variable TWITTER_BEARER_TOKEN not found")
-	// }
-	// user := getUserByUserName(userToCheck)
-	// baseUrl := fmt.Sprintf("https://api.twitter.com/2/users/%v/followers", user.Data.Id)
-	// body := makeRequest("GET", baseUrl, bearerToken)
-	// var twitterData TwitterAPIResponseFollowers
-	// err := json.Unmarshal([]byte(body), &twitterData)
-	// handleErr(err)
-	// // When twitter API returns some status, is because the API fails. Weird :(
-	// if twitterData.Status != 0 {
-	// 	log.Fatalln("Cant make request", err)
-	// }
-	// for _, user := range twitterData.Data {
-	// 	if isNameSimilar(user.Name, followedBy) {
-	// 		fmt.Printf("Name: %s Username: %s\n", user.Name, user.UserName)
-	// 	}
-	// }
+	bearerToken := os.Getenv("TWITTER_BEARER_TOKEN")
+	if bearerToken == "" {
+		log.Fatalln("Environment Variable TWITTER_BEARER_TOKEN not found")
+	}
+	numberChoise := getUserChoise()
+	if numberChoise == 1 {
+		fmt.Println("Enter your Tweet message:")
+		reader := bufio.NewReader(os.Stdin)
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSuffix(text, "\n")
+		text = strings.TrimSuffix(text, "\r")
+		body := makeTweet(text)
+		fmt.Println(string(body))
+	} else {
+		userToCheck, followedBy := askUser()
+		user := getUserByUserName(userToCheck)
+		baseUrl := fmt.Sprintf("https://api.twitter.com/2/users/%v/followers", user.Data.Id)
+		body := makeRequest("GET", baseUrl, "Bearer "+bearerToken)
+		var twitterData TwitterAPIResponseFollowers
+		err := json.Unmarshal([]byte(body), &twitterData)
+		handleErr(err)
+		// When twitter API returns some status, is because the API fails. Weird :(
+		if twitterData.Status != 0 {
+			log.Fatalln("Cant make request", err)
+		}
+		for _, user := range twitterData.Data {
+			if isNameSimilar(user.Name, followedBy) {
+				fmt.Printf("Name: %s Username: %s\n", user.Name, user.UserName)
+			}
+		}
+	}
 
 }
